33dd45e5e8cd4eaf2676272674c590d82342357d
chowsh3 <shobir.c@icloud.com>
2020-03-22 05:18:15 +0000

    changes

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
-console.log("rock paper scissors");
+console.log("Welcome rock paper scissors!");
+
+//Obtain a choice from the user
+
+
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+// Validate the users choice and gell them if they made a mistake
+    //strip capitalisation or ignore caps
+function handleUserResponse(response) {
+  if (response !== "rock" && response !== "paper" && response !== "scissors") {
+    console.log("Please enter as shown: rock, paper or scissors");
+    readline.close();
+  } else {
+
+
+    
+
+
+    //Make a random selection for the computer
+    randomSelection = Math.floor(Math.random() * 3);
+    //A radnom  choice of rock paper or scissors-how to make the pc make a random choice
+
+    if (randomSelection === 1) {
+      randomSelection = "rock";
+    } else if (randomSelection === 2) {
+      randomSelection = "paper";
+    } else {
+      randomSelection = "scissors";
+    }
+    console.log(randomSelection);
+    //compare the two choices
+
+    var compare = function(response, randomSelection) {
+      if (response === randomSelection) {
+        console.log("DRAW");
+      } else if (response === "rock") {
+        if (randomSelection === "scissors") {
+          console.log("rock wins");
+        } else console.log("papers wins");
+
+      } else if (response === "paper") {
+        if (randomSelection === "rock") {
+          console.log("paper wins");
+        } else {
+          console.log("scissors wins");
+        }
+      }
+    };
+
+    compare(response, randomSelection);
+
+
+   
+    readline.close();
+
+  }
+}
+readline.question("Please choose either rock, paper or scissors!\n", handleUserResponse);
